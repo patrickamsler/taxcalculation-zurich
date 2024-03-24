0b739fa3e14e491957e56e2c4829fe6ca3c0924f
Patrick Amsler <patrick.amsler@abraxas.ch>
2024-03-24 13:59:55 +0100

    enums for tarif and taxtype introduced

--- taxcalcalculation_zh.py
+++ taxcalcalculation_zh.py
@@ -1,8 +1,18 @@
 import pandas as pd
+from enum import Enum
 
-def select_tariftabelle(year, tax_type, tarif):
+class TaxType(Enum):
+    EINKOMMEN = 1
+    VERMOEGEN = 2
+    
+class Tarif(Enum):
+    GRUNDTARIF = 1
+    VERHEIRATETENTARIF = 2
+
+
+def select_tariftabelle(year, tax_type: TaxType, tarif: Tarif):
     # Construct the filename
-    filename = f"./tariftabellen/{year}-{tax_type}-{tarif}.csv"
+    filename = f"./tariftabellen/{year}-{tax_type.name.lower()}-{tarif.name.lower()}.csv"
     names_einkommen = ['Einkommensbereich (CHF)', 'Steuer Grundtarif (CHF)', 'Zusätzlicher Steuerbetrag pro 100 CHF']
     names_vermoegen = ['steuerbares Vermögen (CHF)', 'Steuer (CHF)', 'für je weitere 1000. Vemögen']
     names = names_einkommen if tax_type == 'einkommen' else names_vermoegen
@@ -14,11 +24,9 @@ def select_tariftabelle(year, tax_type, tarif):
         print(f"File {filename} not found. Please check the file path.")
         return None
 
-# Use the function
-year = 2018
-tax_type = 'einkommen'
-# tax_type = 'vermoegen'
-# tarif = 'verheiratetentarif'
-tarif = 'grundtarif'
-data = select_tariftabelle(year, tax_type, tarif)
+data = select_tariftabelle(
+    year = 2018, 
+    tax_type = TaxType.EINKOMMEN,
+    tarif =Tarif.VERHEIRATETENTARIF,
+)
 print(data)
