db3459df6673fb6bcea1b83c399dbe0cc2a12bd6
Patrick Amsler <patrick.amsler@abraxas.ch>
2024-03-24 15:35:56 +0100

    tax calculation

--- taxcalcalculation_zh.py
+++ taxcalcalculation_zh.py
@@ -25,11 +25,9 @@ def select_tariftabelle(year, tax_type: TaxType, tarif: Tarif):
         return None
     
 def calculate_income_tax(year, tarif: Tarif, income):
-    # Select the tax table
     data = select_tariftabelle(year, TaxType.EINKOMMEN, tarif)
-    # select first entry
     first_bracket = data.iloc[0]
-    if income <= first_bracket[1]:
+    if income <= first_bracket.iloc[0]:
         return 0
     
     current_bracket = first_bracket
@@ -39,20 +37,34 @@ def calculate_income_tax(year, tarif: Tarif, income):
         else:
             current_bracket = bracket
     return current_bracket[2] + (income - current_bracket[1]) / 100 * current_bracket[3]
+
+def calculate_wealth_tax(year, tarif: Tarif, wealth):
+    data = select_tariftabelle(year, TaxType.VERMOEGEN, tarif)
+    first_bracket = data.iloc[0]
+    if wealth <= first_bracket.iloc[0]:
+        return 0
+    current_bracket = first_bracket
+    for bracket in data.itertuples():
+        if wealth < bracket[1]:
+            break
+        else:
+            current_bracket = bracket
+    return current_bracket[2] + (wealth - current_bracket[1]) / 1000 * current_bracket[3]
     
 
 income = 100_000
+wealth = 385_000
+
 income_tax_grundtarif = calculate_income_tax(2018, Tarif.GRUNDTARIF, income)
 income_tax_verheiratetentarif = calculate_income_tax(2018, Tarif.VERHEIRATETENTARIF, income)
+wealth_tax_grundtarif = calculate_wealth_tax(2018, Tarif.GRUNDTARIF, wealth)
+wealth_tax_verheiratetentarif = calculate_wealth_tax(2018, Tarif.VERHEIRATETENTARIF, wealth)
 
-print("einfache steuer einkommen grundtarif:           ", income_tax_grundtarif)
-print("einfache steuer einkommen verheiratetentarif:   ", income_tax_verheiratetentarif)
-
-
-
-# data = select_tariftabelle(
-#     year = 2018, 
-#     tax_type = TaxType.EINKOMMEN,
-#     tarif =Tarif.VERHEIRATETENTARIF,
-# )
-# print(data)
+print("Einkommen: ", income)
+print("Vermögen: ", wealth)
+print("")
+print("== Einfache Steuer == ")
+print("Einkommen GT: ", income_tax_grundtarif)
+print("Einkommen VT: ", income_tax_verheiratetentarif)
+print("Vermögen GT:  ", wealth_tax_grundtarif)
+print("Vermögen VT:  ", wealth_tax_verheiratetentarif)
