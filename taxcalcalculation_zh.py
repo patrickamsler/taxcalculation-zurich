84ca5c473e715b275e3991eaa5507c72e1b29789
Patrick Amsler <patrick.amsler@abraxas.ch>
2024-03-24 13:51:55 +0100

    tariftabellen for vermoegen added

--- taxcalcalculation_zh.py
+++ taxcalcalculation_zh.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-def select_tariftabelle(year, tax_type, tariff):
+def select_tariftabelle(year, tax_type, tarif):
     # Construct the filename
-    filename = f"./tariftabellen/{year}-{tax_type}-{tariff}.csv"
-
+    filename = f"./tariftabellen/{year}-{tax_type}-{tarif}.csv"
+    names_einkommen = ['Einkommensbereich (CHF)', 'Steuer Grundtarif (CHF)', 'Zusätzlicher Steuerbetrag pro 100 CHF']
+    names_vermoegen = ['steuerbares Vermögen (CHF)', 'Steuer (CHF)', 'für je weitere 1000. Vemögen']
+    names = names_einkommen if tax_type == 'einkommen' else names_vermoegen
     # Read the CSV file
     try:
-        data = pd.read_csv(filename, names=['Einkommensbereich (CHF)', 'Steuer Grundtarif (CHF)', 'Zusätzlicher Steuerbetrag pro 100 CHF'], skiprows=1, header=None)
+        data = pd.read_csv(filename, names=names, skiprows=1, header=None)
         return data
     except FileNotFoundError:
         print(f"File {filename} not found. Please check the file path.")
@@ -15,6 +17,7 @@ def select_tariftabelle(year, tax_type, tariff):
 # Use the function
 year = 2018
 tax_type = 'einkommen'
+# tax_type = 'vermoegen'
 # tarif = 'verheiratetentarif'
 tarif = 'grundtarif'
 data = select_tariftabelle(year, tax_type, tarif)
